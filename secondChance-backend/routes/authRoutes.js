3dcdfbc3c269645e19391bf29a29cda212c11828
MayangAsura <mayang.asura123@gmail.com>
2026-03-11 23:47:14 +0700

    fixing eslint: authRoutes.js

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -1,178 +1,360 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const dotenv = require('dotenv')
-const { check, validationResult } = require('express-validator')
-// body,
-const logger = require('../logger.js')
-// const logger = pino()
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+const { check, validationResult } = require('express-validator');
+const logger = require('../logger.js');
 
-dotenv.config()
+dotenv.config();
 
-const connectToDatabase = require('../models/db.js')
+const connectToDatabase = require('../models/db.js');
 
-const router = express.Router()
+const router = express.Router();
 
 router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
 
-    const { email, password } = req.body
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection('users');
+    const users = await collection.findOne({ email });
 
-    try {
+    if (users) {
+      const isMatch = await bcrypt.compare(password, users.password);
 
-        const db = await connectToDatabase()
+      if (!isMatch) {
+        logger.error('Email or password wrong.');
+        return res.status(400).json({ error: 'Email or Password wrong.' });
+      }
 
-        const collection = await db.collection('users')
+      const { firstName: userName, email: userEmail } = users;
 
-        const users = await collection.findOne({ email: email })
+      const payload = {
+        user: {
+          _id: users._id.toString(),
+        },
+      };
 
-        if (users) {
-            const isMatch = await bcrypt.compare(password, users.password)
+      const JWT_SECRET = process.env.JWT_SECRET;
+      const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: '24h' });
 
-            if (!isMatch) {
-                logger.error('Email or password wrong.')
-                return res.status(400).json({ error: 'Email or Password wrong.' })
-            }
-
-            const userName = users.firstName
-            const userEmail = users.email
-
-            const payload = {
-                user: {
-                    _id: users._id.toString()
-                }
-            }
-            const JWT_SECRET = process.env.JWT_SECRET
-
-            const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: '24h' })
-
-            res.json({ authtoken, userName, userEmail })
-
-        } else {
-            logger.error('User not found')
-            return res.status(404).json({ error: 'User not found' })
-        }
-
-
-    } catch (error) {
-        logger.error('Error when logged in: ' + error)
-        res.status(500).json({ error: 'Error when logged in: '+ error })
+      return res.json({ authtoken, userName, userEmail });
     }
-})
+
+    logger.error('User not found');
+    return res.status(404).json({ error: 'User not found' });
+  } catch (error) {
+    logger.error(`Error when logged in: ${error}`);
+    return res.status(500).json({ error: `Error when logged in: ${error}` });
+  }
+});
 
 router.post('/register', async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection('users');
 
-    try {
-        const db = await connectToDatabase()
-        const collection = await db.collection('users')
+    const { email, password, firstName, lastName } = req.body;
 
-        const { email, password, firstName, lastName } = req.body
-
-        const user = await collection.findOne({ email: req.body.email })
-        if (user) {
-            logger.error('User already exist')
-            return res.status(400).json({ error: 'User already exist.' })
-        }
-
-        const salt = await bcrypt.genSalt(10)
-        const hashed_password = await bcrypt.hash(password, salt)
-
-        const data = await collection.insertOne({
-            email: email,
-            password: hashed_password,
-            firstName: firstName,
-            lastName: lastName,
-            createdAt: new Date().toDateString()
-        })
-
-        const JWT_SECRET = process.env.JWT_SECRET
-        const payload = {
-            user: {
-                id: data.insertedId
-            }
-        }
-        const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: '24h' })
-
-        logger.info('User successfully registered.')
-        res.status(200).json({ email, authtoken })
-
-    } catch (error) {
-        logger.error('Error when register: ' + error)
-        res.status(500).json({ error: 'Error when register: ' + error })
+    const user = await collection.findOne({ email });
+    if (user) {
+      logger.error('User already exist');
+      return res.status(400).json({ error: 'User already exist.' });
     }
 
-})
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-router.put('/update',
-    [
-        check('email').trim().escape().notEmpty().withMessage('Last name is required.').isEmail().withMessage('Please input valid email').custom( async (email) => {
-            try {
-                const collection = await (await connectToDatabase()).collection('users')
-                const user = await collection.findOne({ email: email })
-                if (!user) {
-                    return Promise.reject('Email not found')
-                }
+    const data = await collection.insertOne({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+      createdAt: new Date().toDateString(),
+    });
 
-            } catch (error) {
-                throw error
-            }
-        }),
-        check('name').trim().escape().notEmpty().withMessage('Name is required.')
-        // check('lastName').trim().escape().notEmpty().withMessage('Last name is required.'),
-        // check('password').trim().escape().notEmpty().withMessage('Password is required.').isStrongPassword({
-        //     minLength: 8,
-        //     minUppercase: 1,
-        //     minLowercase: 1,
-        //     minSymbols: 0
-        // }).withMessage("Please ensure your password at least 8 characters long, has at least one uppercase and one lowercase letters"),
-        // check('confirm_password').trim().escape().notEmpty().withMessage("Confirm passwors is required.").custom(
-        //     (confirmPassword, {req: request}) => confirmPassword== request.body.password
-        // ).withMessage('The password and its confirm password do not match, please input correctly')
-    ],
-    async (req, res) => {
+    const JWT_SECRET = process.env.JWT_SECRET;
+    const payload = {
+      user: {
+        id: data.insertedId,
+      },
+    };
+    const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: '24h' });
+
+    logger.info('User successfully registered.');
+    return res.status(200).json({ email, authtoken });
+  } catch (error) {
+    logger.error(`Error when register: ${error}`);
+    return res.status(500).json({ error: `Error when register: ${error}` });
+  }
+});
+
+router.put(
+  '/update',
+  [
+    check('email')
+      .trim()
+      .escape()
+      .notEmpty()
+      .withMessage('Email is required.')
+      .isEmail()
+      .withMessage('Please input valid email')
+      .custom(async (email) => {
+        try {
+          const collection = (await connectToDatabase()).collection('users');
+          const user = await collection.findOne({ email });
+          if (!user) {
+            throw new Error('Email not found');
+          }
+          return true;
+        } catch (error) {
+          throw new Error(error.message);
+        }
+      }),
+    check('name').trim().escape().notEmpty().withMessage('Name is required.'),
+  ],
+  async (req, res) => {
     try {
-        const { name } = req.body
+      const { name } = req.body;
 
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            logger.error('Error validation: ', errors.array())
-            return res.status(400).json({ error: `Validation error: ${errors.array()}` })
-        }
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        logger.error('Error validation: ', errors.array());
+        return res
+          .status(400)
+          .json({ error: `Validation error: ${errors.array()}` });
+      }
 
-        const email = req.headers.email
+      const email = req.headers.email;
 
-        if (!email) {
-            logger.error('Error email not include in request headers')
-            return res.status(400).json({ error: 'Please include email in request headers' })
-        }
-        const collection = (await connectToDatabase()).collection('users')
-        const users = await collection.findOne({ email: email })
-        // const salt = bcrypt.genSalt(10)
-        // const hash_password = bcrypt.hash(password, salt)
-        users.firstName = name
-        // users.lastName = lastName
-        users.email = email
-        // users.password = hash_password
-        users.updatedAt = new Date().toDateString()
+      if (!email) {
+        logger.error('Error email not include in request headers');
+        return res
+          .status(400)
+          .json({ error: 'Please include email in request headers' });
+      }
 
-        const updatedUser = await collection.findOneAndUpdate(
-            { email },
-            { $set: users },
-            { returnDocument: 'after' },
-        )
-        const payload = {
-            user: {
-                id: updatedUser._id.toString()
-            }
-        }
-        const JWT_SECRET = process.env.JWT_SECRET
-        const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: '24h' })
+      const db = await connectToDatabase();
+      const collection = db.collection('users');
+      const users = await collection.findOne({ email });
 
-        res.status(200).json({ message: 'Succesfuly updated profile', authtoken })
+      if (!users) {
+        logger.error('User not found');
+        return res.status(404).json({ error: 'User not found' });
+      }
 
+      users.firstName = name;
+      users.updatedAt = new Date().toDateString();
+
+      const updatedUser = await collection.findOneAndUpdate(
+        { email },
+        { $set: users },
+        { returnDocument: 'after' },
+      );
+
+      const payload = {
+        user: {
+          id: updatedUser._id.toString(),
+        },
+      };
+
+      const JWT_SECRET = process.env.JWT_SECRET;
+      const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: '24h' });
+
+      return res
+        .status(200)
+        .json({ message: 'Successfully updated profile', authtoken });
     } catch (error) {
-        logger.error('Error when update user.' + error)
-        return res.status(400).json({ error: 'Error when update user.' + error })
+      logger.error(`Error when update user: ${error}`);
+      return res.status(400).json({ error: `Error when update user: ${error}` });
     }
-})
+  },
+);
 
-module.exports = router
+module.exports = router;
+// const express = require("express");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
+// const dotenv = require("dotenv");
+// const { check, validationResult } = require("express-validator");
+// // body,
+// const logger = require("../logger.js");
+// // const logger = pino()
+
+// dotenv.config();
+
+// const connectToDatabase = require("../models/db.js");
+
+// const router = express.Router();
+
+// router.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+
+//   try {
+//     const db = await connectToDatabase();
+
+//     const collection = await db.collection("users");
+
+//     const users = await collection.findOne({ email: email });
+
+//     if (users) {
+//       const isMatch = await bcrypt.compare(password, users.password);
+
+//       if (!isMatch) {
+//         logger.error("Email or password wrong.");
+//         return res.status(400).json({ error: "Email or Password wrong." });
+//       }
+
+//       const userName = users.firstName;
+//       const userEmail = users.email;
+
+//       const payload = {
+//         user: {
+//           _id: users._id.toString(),
+//         },
+//       };
+
+//       const JWT_SECRET = process.env.JWT_SECRET;
+
+//       const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: "24h" });
+
+//       res.json({ authtoken, userName, userEmail });
+//     } else {
+//       logger.error("User not found");
+//       return res.status(404).json({ error: "User not found" });
+//     }
+//   } catch (error) {
+//     logger.error("Error when logged in: " + error);
+//     res.status(500).json({ error: "Error when logged in: " + error });
+//   }
+// });
+
+// router.post("/register", async (req, res) => {
+//   try {
+//     const db = await connectToDatabase();
+//     const collection = await db.collection("users");
+
+//     const { email, password, firstName, lastName } = req.body;
+
+//     const user = await collection.findOne({ email: req.body.email });
+//     if (user) {
+//       logger.error("User already exist");
+//       return res.status(400).json({ error: "User already exist." });
+//     }
+
+//     const salt = await bcrypt.genSalt(10);
+//     const hashed_password = await bcrypt.hash(password, salt);
+
+//     const data = await collection.insertOne({
+//       email: email,
+//       password: hashed_password,
+//       firstName: firstName,
+//       lastName: lastName,
+//       createdAt: new Date().toDateString(),
+//     });
+
+//     const JWT_SECRET = process.env.JWT_SECRET;
+//     const payload = {
+//       user: {
+//         id: data.insertedId,
+//       },
+//     };
+//     const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: "24h" });
+
+//     logger.info("User successfully registered.");
+//     res.status(200).json({ email, authtoken });
+//   } catch (error) {
+//     logger.error("Error when register: " + error);
+//     res.status(500).json({ error: "Error when register: " + error });
+//   }
+// });
+
+// router.put(
+//   "/update",
+//   [
+//     check("email")
+//       .trim()
+//       .escape()
+//       .notEmpty()
+//       .withMessage("Last name is required.")
+//       .isEmail()
+//       .withMessage("Please input valid email")
+//       .custom(async (email) => {
+//         try {
+//           const collection = await (
+//             await connectToDatabase()
+//           ).collection("users");
+//           const user = await collection.findOne({ email: email });
+//           if (!user) {
+//             return Promise.reject("Email not found");
+//           }
+//         } catch (error) {
+//           throw error;
+//         }
+//       }),
+//     check("name").trim().escape().notEmpty().withMessage("Name is required."),
+//     // check('lastName').trim().escape().notEmpty().withMessage('Last name is required.'),
+//     // check('password').trim().escape().notEmpty().withMessage('Password is required.').isStrongPassword({
+//     //     minLength: 8,
+//     //     minUppercase: 1,
+//     //     minLowercase: 1,
+//     //     minSymbols: 0
+//     // }).withMessage("Please ensure your password at least 8 characters long, has at least one uppercase and one lowercase letters"),
+//     // check('confirm_password').trim().escape().notEmpty().withMessage("Confirm passwors is required.").custom(
+//     //     (confirmPassword, {req: request}) => confirmPassword== request.body.password
+//     // ).withMessage('The password and its confirm password do not match, please input correctly')
+//   ],
+//   async (req, res) => {
+//     try {
+//       const { name } = req.body;
+
+//       const errors = validationResult(req);
+//       if (!errors.isEmpty()) {
+//         logger.error("Error validation: ", errors.array());
+//         return res
+//           .status(400)
+//           .json({ error: `Validation error: ${errors.array()}` });
+//       }
+
+//       const email = req.headers.email;
+
+//       if (!email) {
+//         logger.error("Error email not include in request headers");
+//         return res
+//           .status(400)
+//           .json({ error: "Please include email in request headers" });
+//       }
+//       const collection = (await connectToDatabase()).collection("users");
+//       const users = await collection.findOne({ email: email });
+//       // const salt = bcrypt.genSalt(10)
+//       // const hash_password = bcrypt.hash(password, salt)
+//       users.firstName = name;
+//       // users.lastName = lastName
+//       users.email = email;
+//       // users.password = hash_password
+//       users.updatedAt = new Date().toDateString();
+
+//       const updatedUser = await collection.findOneAndUpdate(
+//         { email },
+//         { $set: users },
+//         { returnDocument: "after" },
+//       );
+//       const payload = {
+//         user: {
+//           id: updatedUser._id.toString(),
+//         },
+//       };
+//       const JWT_SECRET = process.env.JWT_SECRET;
+//       const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: "24h" });
+
+//       res
+//         .status(200)
+//         .json({ message: "Succesfuly updated profile", authtoken });
+//     } catch (error) {
+//       logger.error("Error when update user." + error);
+//       return res.status(400).json({ error: "Error when update user." + error });
+//     }
+//   },
+// );
+
+// module.exports = router;
