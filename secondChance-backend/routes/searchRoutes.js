9d616afaf8bc8a575c0cc9260da7717b5e939286
MayangAsura <mayang.asura123@gmail.com>
2026-03-23 16:45:29 +0700

    fixing eslint: sentiment/index.js searchRoute.js

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -4,43 +4,90 @@ const connectToDatabase = require('../models/db')
 
 // Search for gifts
 router.get('/', async (req, res, next) => {
-    try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
-        // {{insert code here}}
-        const db = await connectToDatabase()
+  try {
+    // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
+    // {{insert code here}}
+    const db = await connectToDatabase()
 
-        const collection = db.collection('secondChanceItems')
+    const collection = db.collection('secondChanceItems')
 
-        // Initialize the query object
-        const query = {}
+    // Initialize the query object
+    const query = {}
 
-        // Add the name filter to the query if the name parameter is not empty
-        if (req.query.name && req.query.name.trim() !== '') {
-            query.name = { $regex: req.query.name, $options: 'i' } // Using regex for partial match, case-insensitive
-        }
-
-        // Task 3: Add other filters to the query
-        if (req.query.category) {
-            // {{insert code here}}
-            query.category = req.query.category
-        }
-        if (req.query.condition) {
-            // {{insert code here}}
-            query.condition = req.query.condition
-        }
-        if (req.query.age_years) {
-            // {{insert code here}}
-            query.age_years = { $lte: parseInt(req.query.age_years) }
-        }
-
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        // {{insert code here here}}
-        const secondChanceItems = await collection.find(query).toArray()
-
-        res.json(secondChanceItems)
-    } catch (e) {
-        next(e)
+    // Add the name filter to the query if the name parameter is not empty
+    if (req.query.name && req.query.name.trim() !== '') {
+      query.name = { $regex: req.query.name, $options: 'i' } // Using regex for partial match, case-insensitive
     }
+
+    // Task 3: Add other filters to the query
+    if (req.query.category) {
+      // {{insert code here}}
+      query.category = req.query.category
+    }
+    if (req.query.condition) {
+      // {{insert code here}}
+      query.condition = req.query.condition
+    }
+    if (req.query.age_years) {
+      // {{insert code here}}
+      query.age_years = { $lte: parseInt(req.query.age_years) }
+    }
+
+    // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
+    // {{insert code here here}}
+    const secondChanceItems = await collection.find(query).toArray()
+
+    res.json(secondChanceItems)
+  } catch (e) {
+    next(e)
+  }
 })
 
 module.exports = router
+
+// const express = require('express')
+// const router = express.Router()
+// const connectToDatabase = require('../models/db')
+
+// // Search for gifts
+// router.get('/', async (req, res, next) => {
+//     try {
+//         // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
+//         // {{insert code here}}
+//         const db = await connectToDatabase()
+
+//         const collection = db.collection('secondChanceItems')
+
+//         // Initialize the query object
+//         const query = {}
+
+//         // Add the name filter to the query if the name parameter is not empty
+//         if (req.query.name && req.query.name.trim() !== '') {
+//             query.name = { $regex: req.query.name, $options: 'i' } // Using regex for partial match, case-insensitive
+//         }
+
+//         // Task 3: Add other filters to the query
+//         if (req.query.category) {
+//             // {{insert code here}}
+//             query.category = req.query.category
+//         }
+//         if (req.query.condition) {
+//             // {{insert code here}}
+//             query.condition = req.query.condition
+//         }
+//         if (req.query.age_years) {
+//             // {{insert code here}}
+//             query.age_years = { $lte: parseInt(req.query.age_years) }
+//         }
+
+//         // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
+//         // {{insert code here here}}
+//         const secondChanceItems = await collection.find(query).toArray()
+
+//         res.json(secondChanceItems)
+//     } catch (e) {
+//         next(e)
+//     }
+// })
+
+// module.exports = router
